fc1f3e3c6f234cd07da3d5a62032b1d6385cacb6
xyyojl <m13631897256@163.com>
2019-08-09 16:23:50 +0800

    开始创建 PostList

---
+++ vue-cnode1/src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <div class="loading" v-if="isLoader">
+           <img src="../assets/loading.gif" alt="">
+       </div>
+       <div class="listStyle">
+           <ul>
+               <li>xxx</li>
+           </ul>
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  data () {
+    return {
+      isLoader: false
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
