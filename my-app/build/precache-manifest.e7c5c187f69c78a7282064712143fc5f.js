518f3c968eac8030f1fba962b6be378fdcb8a493
xyyojl <m13631897256@163.com>
2019-09-26 23:20:07 +0800

    创建 React 应用

---
+++ my-app/build/precache-manifest.e7c5c187f69c78a7282064712143fc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54e2e3d2f38ef645eef4bb6c649fd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec4831d76242081f20d5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d5719d780173e27db77f",
+    "url": "/static/js/2.8567c72f.chunk.js"
+  },
+  {
+    "revision": "ec4831d76242081f20d5",
+    "url": "/static/js/main.8444cc56.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  }
+]);
